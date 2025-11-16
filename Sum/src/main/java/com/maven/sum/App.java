e57b6e19b4d867b0f631681f2c0fd1654d3abbd4
debid <debid@DELL>
2025-11-16 10:48:17 +0530

    tl create

---
+++ Sum/src/main/java/com/maven/sum/App.java
@@ -0,0 +1,12 @@
+package com.maven.sum;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Sum is "+(a+b));
+    }
+}
